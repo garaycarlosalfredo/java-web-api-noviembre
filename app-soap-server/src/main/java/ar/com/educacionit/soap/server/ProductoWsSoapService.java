b2f5d8b877b22e573b6e57ad365f131ab5e60076
garaycarlosalfredo <68791092+garaycarlosalfredo@users.noreply.github.com>
2020-11-08 10:57:30 -0300

    Clase 2 visto de video

---
+++ app-soap-server/src/main/java/ar/com/educacionit/soap/server/ProductoWsSoapService.java
@@ -0,0 +1,13 @@
+package ar.com.educacionit.soap.server;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+import ar.com.educacionit.domain.Producto;
+import ar.com.educacionit.soap.dto.CreateProductoDTO;
+
+@WebService()
+public interface ProductoWsSoapService {
+	@WebMethod
+	public Producto createProducto(CreateProductoDTO createProductoRequest);
+}
